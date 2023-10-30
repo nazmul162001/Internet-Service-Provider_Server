6e3f4a0a363e0d4aa3442d1021dbd05d4b10903f
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-10-30 23:22:23 +0600

    added relation blogs with user

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -25,7 +25,7 @@ router.patch(
 
 router.delete(
   '/users/:id',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER, ENUM_USER_ROLE.SUPER_ADMIN),
   UserController.deleteSingleUser
 );
 
